b7d14f6bedd4d3208f15fde66b7fc6354c83de81
fortune00 <sinyoungbok99@gmail.com>
2022-09-20 20:10:55 +0900

    [chore] Kafka 프로젝트 초기화

---
+++ src/main/java/fortune/KafkaexamApplication.java
@@ -0,0 +1,13 @@
+package fortune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaexamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fortune/KafkaexamApplicationTests.java
@@ -0,0 +1,13 @@
+package fortune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
